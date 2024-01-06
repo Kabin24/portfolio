f5f866ea1729d1451846a0db158bebd911b97b9d
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2024-01-06 16:37:55 +0545

    changed

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Kabin_Resume.pdf", 
+      href: "/Kabinstha_Resume.pdf", 
       style: "rounded-br-md",
       download: true,
     },
